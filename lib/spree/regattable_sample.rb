6774c2b36da22ae7c0b0933efa79a3fbc7e27750
Miguel Sancho <sanchojaf@gmail.com>
2014-01-06 15:30:37 -0500

    change some elements

---
+++ lib/spree/regattable_sample.rb
@@ -0,0 +1,17 @@
+module Spree
+  module RegattableSample
+    def self.load_regattable_sample(file)
+      path = File.expand_path(regattable_samples_path + "#{file}.rb")
+      # Check to see if the specified file has been loaded before
+      if !$LOADED_FEATURES.include?(path)
+        require path
+        puts "Loaded #{file.titleize} regattable samples"
+      end
+    end
+
+    private
+      def self.samples_regattable_path
+        Pathname.new(File.join(File.dirname(__FILE__), '..', '..', 'db', 'regattable_samples'))
+      end
+  end
+end
