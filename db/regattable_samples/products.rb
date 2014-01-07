65e9474b5ebbd829969730c6a4e4634ee259de8f
Miguel Sancho <sanchojaf@gmail.com>
2014-01-06 22:49:59 -0500

    change properties file

--- db/regattable_samples/products.rb
+++ db/regattable_samples/products.rb
@@ -5,7 +5,6 @@ clothing = Spree::TaxCategory.find_by_name!("Clothing")
 shipping_category = Spree::ShippingCategory.find_by_name!("Default")
 
 default_attrs = {
-  :description => Faker::Lorem.paragraph,
   :available_on => Time.zone.now
 }
 
@@ -16,6 +15,7 @@ products = [
     :shipping_category => shipping_category,
     :price => 15.99,
     :eur_price => 14,
+    :description => Faker::Lorem.paragraph,
   },
   {
     :name => "Ruby on Rails Bag",
@@ -23,13 +23,15 @@ products = [
     :shipping_category => shipping_category,
     :price => 22.99,
     :eur_price => 19,
+    :description => Faker::Lorem.paragraph,
   },
   {
     :name => "FC-16",
     :tax_category => clothing,
     :shipping_category => shipping_category,
     :price => 19.99,
-    :eur_price => 16
+    :eur_price => 16,
+    :description => "Compared to most ordinary sailboats, our revolutionary Patent Pending Folding Hull Technology      makes a portable 16ft catamaran which can be checked on a plane, stored under a bed, or raced in virtually any body of water.Every component breaks into sections that are less that 4ft long, allowing you to fit the entire sailboat into 2 large suitcases, with an estimated size of 4 x 3.5 x 1.25 each. With an estimated weight of 75 lbs (~34 kg) per suitcase, we put large wheels on the bottom for easy transport.The suitcases can be attached together for single person travel, or kept apart for sharing the load, airline use, or storing it under your bed",
   },
   {
     :name => "Ruby on Rails Jr. Spaghetti",
@@ -37,6 +39,7 @@ products = [
     :shipping_category => shipping_category,
     :price => 19.99,
     :eur_price => 16
+    :description => Faker::Lorem.paragraph,
 
   },
   {
@@ -45,6 +48,7 @@ products = [
     :tax_category => clothing,
     :price => 19.99,
     :eur_price => 16
+    :description => Faker::Lorem.paragraph,
   },
   {
     :name => "Ruby Baseball Jersey",
@@ -52,6 +56,7 @@ products = [
     :shipping_category => shipping_category,
     :price => 19.99,
     :eur_price => 16
+    :description => Faker::Lorem.paragraph,
   },
   {
     :name => "Apache Baseball Jersey",
@@ -59,6 +64,7 @@ products = [
     :shipping_category => shipping_category,
     :price => 19.99,
     :eur_price => 16
+    :description => Faker::Lorem.paragraph,
   },
   {
     :name => "Spree Baseball Jersey",
@@ -66,6 +72,7 @@ products = [
     :shipping_category => shipping_category,
     :price => 19.99,
     :eur_price => 16
+    :description => Faker::Lorem.paragraph,
   },
   {
     :name => "Spree Jr. Spaghetti",
@@ -73,6 +80,7 @@ products = [
     :shipping_category => shipping_category,
     :price => 19.99,
     :eur_price => 16
+    :description => Faker::Lorem.paragraph,
   },
   {
     :name => "Spree Ringer T-Shirt",
@@ -80,6 +88,7 @@ products = [
     :shipping_category => shipping_category,
     :price => 19.99,
     :eur_price => 16
+    :description => Faker::Lorem.paragraph,
   },
   {
     :name => "Spree Tote",
@@ -87,6 +96,7 @@ products = [
     :shipping_category => shipping_category,
     :price => 15.99,
     :eur_price => 14,
+    :description => Faker::Lorem.paragraph,
   },
   {
     :name => "Spree Bag",
@@ -94,30 +104,35 @@ products = [
     :shipping_category => shipping_category,
     :price => 22.99,
     :eur_price => 19
+    :description => Faker::Lorem.paragraph,
   },
   {
     :name => "Ruby on Rails Mug",
     :shipping_category => shipping_category,
     :price => 13.99,
     :eur_price => 12
+    :description => Faker::Lorem.paragraph,
   },
   {
     :name => "Ruby on Rails Stein",
     :shipping_category => shipping_category,
     :price => 16.99,
     :eur_price => 14
+    :description => Faker::Lorem.paragraph,
   },
   {
     :name => "Spree Stein",
     :shipping_category => shipping_category,
     :price => 16.99,
     :eur_price => 14,
+    :description => Faker::Lorem.paragraph,
   },
   {
     :name => "Spree Mug",
     :shipping_category => shipping_category,
     :price => 13.99,
     :eur_price => 12
+    :description => Faker::Lorem.paragraph,
   }
 ]
 
