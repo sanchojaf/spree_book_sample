563ff5d9dc1e03d0463e9f93eef0b7d230140d56
Miguel Sancho <sanchojaf@gmail.com>
2014-03-30 17:16:41 -0400

    fixed load image products

--- db/peach_samples/assets.rb
+++ db/peach_samples/assets.rb
@@ -63,7 +63,7 @@ images = {
 #  ],
   products[:sleeveless_tee].master => [
     {
-      :attachment => image('sleeveless_tee')
+      :attachment => image(:sleeveless_tee)
     }
   ],
 #  products[:full].master => [ 
@@ -73,7 +73,7 @@ images = {
 #  ],
   products[:spaghetti_strap_cami].master => [
     {
-      :attachment => image('spaghetti_strap_cami')
+      :attachment => image(:spaghetti_strap_cami)
     }
   ],
 #  products[:bikini].maste => [
@@ -93,37 +93,37 @@ images = {
 #  ],
   products[:wrap_cami].master => [
     {
-      :attachment => image('wrap_cami')
+      :attachment => image(:wrap_cami)
     },
   ],
   products[:wrap_chemise].master => [
     {
-      :attachment => image('wrap_chemise')
+      :attachment => image(:wrap_chemise)
     },
   ],
   products[:everyday_pant].master => [
     {
-      :attachment => image('everyday_pant')
+      :attachment => image(:everyday_pant)
     },
   ],
   products[:relaxed_legging].master => [
     {
-      :attachment => image('relaxed_legging')
+      :attachment => image(:relaxed_legging)
     },
   ],
   products[:everyday_jacket].master => [
     {
-      :attachment => image('everyday_jacket')
+      :attachment => image(:everyday_jacket)
     },
   ],
   products[:scoop_neck_cami].master => [
     {
-      :attachment => image('scoop_neck_cami')
+      :attachment => image(:scoop_neck_cami)
     }
   ],
   products[:flyaway_jacket].master => [
     {
-      :attachment => image('flyaway_jacket')
+      :attachment => image(:flyaway_jacket)
     },
   ],
 #  products[:girlshort].master => [
