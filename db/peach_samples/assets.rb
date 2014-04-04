24b1350d3452187e4e3e14fc5de111123e59e0a7
Miguel Sancho <sanchojaf@gmail.com>
2014-04-04 16:51:23 -0400

    changed const key by string in dic in assets load

--- db/peach_samples/assets.rb
+++ db/peach_samples/assets.rb
@@ -1,22 +1,21 @@
 #Spree::Sample.load_sample("products")
 #Spree::Sample.load_sample("variants")
 
-products = {}
-
-products['refined_spaghetti_strap_cami'] = Spree::Product.find_by_name!("Refined Spaghetti Strap Cami") 
-products['refined_1_5_inch_scoop_neck_cami'] = Spree::Product.find_by_name!("Refined 1.5-inch Scoop Neck Cami")
-products['refined_1_5_inch_rib_knit_cami'] = Spree::Product.find_by_name!("Refined 1.5-inch Rib-knit Cami") 
-products['refined_3_inch_scoop_neck_cami'] = Spree::Product.find_by_name!("Refined 3-inch Scoop Neck Cami") 
-products['refined_wrap_cami'] = Spree::Product.find_by_name!("Refined Wrap Cami")
-products['refined_matchstick_pant'] = Spree::Product.find_by_name!("Refined Straight Pant")
-products['refined_straight_pant'] = Spree::Product.find_by_name!("Refined Straight Pant")
-products['refined_wrap_chemise'] = Spree::Product.find_by_name!("Refined Wrap Chemise")
-products['the_jennifer_wrap_jacket'] = Spree::Product.find_by_name!("The Jennifer Wrap Jacket") 
-products['the_carolina_fitted_jacket'] = Spree::Product.find_by_name!("The Carolina Fitted Jacket") 
+products = {
+  'refined_spaghetti_strap_cami' => Spree::Product.find_by_name!("Refined Spaghetti Strap Cami"),
+  'refined_1_5_inch_scoop_neck_cami' => Spree::Product.find_by_name!("Refined 1.5-inch Scoop Neck Cami"),
+  'refined_1_5_inch_rib_knit_cami' => Spree::Product.find_by_name!("Refined 1.5-inch Rib-knit Cami"), 
+  'refined_3_inch_scoop_neck_cami' => Spree::Product.find_by_name!("Refined 3-inch Scoop Neck Cami"), 
+  'refined_wrap_cami' => Spree::Product.find_by_name!("Refined Wrap Cami"),
+  'refined_matchstick_pant' => Spree::Product.find_by_name!("Refined Straight Pant"),
+  'refined_straight_pant' => Spree::Product.find_by_name!("Refined Straight Pant"),
+  'refined_wrap_chemise' => Spree::Product.find_by_name!("Refined Wrap Chemise"),
+  'the_jennifer_wrap_jacket' => Spree::Product.find_by_name!("The Jennifer Wrap Jacket"), 
+  'the_carolina_fitted_jacket' => Spree::Product.find_by_name!("The Carolina Fitted Jacket"), 
+}
 
 def image(name, type="jpg")
   dic_images = {
-
       :refined_spaghetti_strap_cami => 'C-01Black',
       :refined_1_5_inch_scoop_neck_cami =>  'C-02White',
       :refined_1_5_inch_rib_knit_cami => 'T-01White',
@@ -27,7 +26,6 @@ def image(name, type="jpg")
       :refined_wrap_chemise => 'D-01Black',
       :the_jennifer_wrap_jacket => 'J-02Pink',
       :the_carolina_fitted_jacket => 'J-01Black'
-
   }
 
   images_path = Pathname.new(File.dirname(__FILE__)) + "images"
@@ -38,8 +36,8 @@ end
 
 images = {}
 
-products.each do |k, p|
- images[p.master] = image(k) 
+products.each do |key,p|
+ images[p.master] = image(key) 
 end
 
 puts "images #{image.inspect}"
