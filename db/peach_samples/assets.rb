da293c0161d5d10793d1878413ec79e1bdf37be7
Miguel Sancho <sanchojaf@gmail.com>
2014-04-06 18:10:44 -0400

    fixed load all assets

--- db/peach_samples/assets.rb
+++ db/peach_samples/assets.rb
@@ -4,29 +4,29 @@
 #Spree::Sample.load_sample("variants")
 
 products = {
-#  'refined_spaghetti_strap_cami' => Spree::Product.find_by_name!("Refined Spaghetti Strap Cami"),
-#  'refined_1_5_inch_scoop_neck_cami' => Spree::Product.find_by_name!("Refined 1.5-inch Scoop Neck Cami"),
+  'refined_spaghetti_strap_cami' => Spree::Product.find_by_name!("Refined Spaghetti Strap Cami"),
+  'refined_1_5_inch_scoop_neck_cami' => Spree::Product.find_by_name!("Refined 1.5-inch Scoop Neck Cami"),
   'refined_1_5_inch_rib_knit_cami' => Spree::Product.find_by_name!("Refined 1.5-inch Rib-knit Cami"), 
-#  'refined_3_inch_scoop_neck_cami' => Spree::Product.find_by_name!("Refined 3-inch Scoop Neck Cami"), 
-#  'refined_wrap_cami' => Spree::Product.find_by_name!("Refined Wrap Cami"),
-#  'refined_matchstick_pant' => Spree::Product.find_by_name!("Refined Matchstick Pant"),
-#  'refined_straight_pant' => Spree::Product.find_by_name!("Refined Straight Pant"),
-#  'refined_wrap_chemise' => Spree::Product.find_by_name!("Refined Wrap Chemise"),
-#  'the_jennifer_wrap_jacket' => Spree::Product.find_by_name!("The Jennifer Wrap Jacket"), 
-#  'the_carolina_fitted_jacket' => Spree::Product.find_by_name!("The Carolina Fitted Jacket"), 
+  'refined_3_inch_scoop_neck_cami' => Spree::Product.find_by_name!("Refined 3-inch Scoop Neck Cami"), 
+  'refined_wrap_cami' => Spree::Product.find_by_name!("Refined Wrap Cami"),
+  'refined_matchstick_pant' => Spree::Product.find_by_name!("Refined Matchstick Pant"),
+  'refined_straight_pant' => Spree::Product.find_by_name!("Refined Straight Pant"),
+  'refined_wrap_chemise' => Spree::Product.find_by_name!("Refined Wrap Chemise"),
+  'the_jennifer_wrap_jacket' => Spree::Product.find_by_name!("The Jennifer Wrap Jacket"), 
+  'the_carolina_fitted_jacket' => Spree::Product.find_by_name!("The Carolina Fitted Jacket"), 
 }
 
 dic_images = {
-#      'refined_spaghetti_strap_cami' => 'C-01Black',
-#      'refined_1_5_inch_scoop_neck_cami' => 'C-02White',
-    'refined_1_5_inch_rib_knit_cami' => 'T-01Pink',
-#      'refined_3_inch_scoop_neck_cami' => 'C-03Black',
-#      'refined_wrap_cami' => 'L-02Black',
-#      'refined_matchstick_pant' => 'C-04Eggplant',
-#      'refined_straight_pant' => 'L-01Black',
-#      'refined_wrap_chemise' => 'D-01Black',
-#      'the_jennifer_wrap_jacket' => 'J-02Pink',
-#      'the_carolina_fitted_jacket' => 'J-01Black'
+  'refined_spaghetti_strap_cami' => 'C-01Black',
+  'refined_1_5_inch_scoop_neck_cami' => 'C-02White',
+  'refined_1_5_inch_rib_knit_cami' => 'T-01Pink',
+  'refined_3_inch_scoop_neck_cami' => 'C-03Black',
+  'refined_wrap_cami' => 'L-02Black',
+  'refined_matchstick_pant' => 'C-04Eggplant',
+  'refined_straight_pant' => 'L-01Black',
+  'refined_wrap_chemise' => 'D-01Black',
+  'the_jennifer_wrap_jacket' => 'J-02Pink',
+  'the_carolina_fitted_jacket' => 'J-01Black'
 }
 
 def image(name, type="jpg")
